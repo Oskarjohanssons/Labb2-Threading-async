a8800d9eddcb6b487cbadf08aa95bdbe48f781c9
Oskar Johansson <oskar.tzar@hotmail.com>
2024-04-19 12:01:47 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Threading_async
+{
+    internal class Car
+    {
+        private readonly Random random = new Random();
+        public string Name { get; }
+        public double Distance { get; private set; }
+        public int Speed { get; private set; } = 120;
+        public bool Finished { get; private set; }
+
+        public Car(string name)
+        {
+            Name = name;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{Name} startar....");
+
+            while (Distance < 10) 
+            {
+                Distance += Speed / 3600.0; //Ändrar hastigheten från km/h till km/s
+                Thread.Sleep(1000);
+
+                if (random.Next(1, 51) == 1)
+                    OutOfGas();
+                else if (random.Next(1, 51) <= 2)
+                    FlatTire();
+                else if (random.Next(1, 51) <= 5)
+                    DirtyWindshield();
+                else if (random.Next(1,51) <= 10)
+                    EngineTrouble();
+                else if(random.Next(1,101) <= 10)
+                    SpeedBoost();
+
+            }
+
+            Finished = true;
+            Console.WriteLine($"{Name} kom i mål!!");
+        }
+
+        private void OutOfGas()
+        {
+            Console.WriteLine($"{Name} har slut på bensin och behöver tanka! Stannar i 30 sekunder.");
+            Thread.Sleep(30000);
+        }
+
+        private void FlatTire()
+        {
+            Console.WriteLine($"{Name} fick en punktering och behöver byta däck! Stannar i 20 sekunder.");
+            Thread.Sleep(20000);
+        }
+
+        private void DirtyWindshield()
+        {
+            Console.WriteLine($"{Name} har en fågel på vindrutan och behöver tvätta den! Stannar i 10 sekunder.");
+            Thread.Sleep(10000);
+        }
+
+        private void EngineTrouble()
+        {
+            Console.WriteLine($"{Name} har motorfel och hastigheten sänks med 1 km/h.");
+            Speed -= 1;
+        }
+
+        private void SpeedBoost()
+        {
+            int boostAmount = random.Next(10, 31);
+            Speed += boostAmount;
+            Console.WriteLine($"{Name} fick en hastighetsökning och ökar hastigheten med {boostAmount} km/h!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Labb2_Threading_async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car erminsBil = new Car("Ermins BMW");
+            Car oskarsBil = new Car("Oskars SAAB");
+
+            Thread erminsThread = new Thread(() => erminsBil.Drive());
+            Thread oskarsThread = new Thread(() => oskarsBil.Drive());
+
+            Console.WriteLine("Klara....");
+            Thread.Sleep(300);
+            Console.WriteLine("Färdiga....");
+            Thread.Sleep(300);
+            Console.WriteLine("Gå!!!!");
+            Thread.Sleep(300);
+
+            Console.WriteLine("Tävlingen har börjat!");
+
+            erminsThread.Start();
+            oskarsThread.Start();
+
+            Thread statusThread = new Thread(() =>
+            {
+                while (!erminsBil.Finished || !oskarsBil.Finished)
+                {
+                    if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter) 
+                    {
+                        Console.WriteLine("\nTävlingsstatus:");
+                        Console.WriteLine($"{erminsBil.Name}: {erminsBil.Distance:F2} km, Hastighet: {erminsBil.Speed} km/h");
+                        Console.WriteLine($"{oskarsBil.Name}: {oskarsBil.Distance:F2} km, Hastighet: {oskarsBil.Speed} km/h");
+                    }
+                }
+            });
+
+            statusThread.Start();
+
+            erminsThread.Join();
+            oskarsThread.Join();
+
+            Car winner = erminsBil.Distance > oskarsBil.Distance ? erminsBil : oskarsBil;
+
+
+            Console.WriteLine("\nVinnaren är: ");
+            for (int i = 0; i < 5; i++)
+            {
+                if (i % 2 == 0)
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else 
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+
+                Console.Write(winner.Name.ToUpper() + "!! ");
+                Thread.Sleep(500);
+            }
+            Console.ResetColor(); 
+
+
+            Console.WriteLine("\nAlla bilar har kommit i mål!");
+
+            Console.WriteLine("Race finished!");
+            Console.ReadKey();
+        }
+    }
+}
